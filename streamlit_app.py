9c84b65e56e2126abe49f4acc0bb730e34bf0173
Carol126 <65058456+Carol126@users.noreply.github.com>
2024-01-31 16:37:03 +0800

    Update streamlit_app.py
    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,10 @@ streamlit.header("Build your own fuit smoothie!")
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+## put a pick list
+streamlit.multiselect("pick some fuits:", list(my_fruit_list.index))
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+
+
